7ff298646452417f589f9bf81d1588d6c7364a06
John Doe <ilay.arugi@gmail.com>
2020-10-16 23:39:37 +0300

    without server

--- packages/DM_IL_Server/chat/chat.js
+++
@@ -1,4 +0,0 @@
-mp.events.add("playerChat", (player, text) =>
-{
-	mp.players.broadcast(`!{#3CFF33}(Global Chat) !{#ffffff}${player.name}: ${text}`);
-});
--- packages/DM_IL_Server/player/session/session.js
+++
@@ -1,3 +0,0 @@
-mp.events.add('playerQuit', function(player, exitType, reason){
-    mp.events.call('server:auth:onPlayerLogout', player.name);
-});
